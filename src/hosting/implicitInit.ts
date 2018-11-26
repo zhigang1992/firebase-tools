4158078618044b1875f3f7b0b450fe0395b8bbde
Bryan Kendall <bkend@google.com>
2018-11-26 15:21:17 -0800

    adding a type for the options object

--- src/hosting/implicitInit.ts
+++ src/hosting/implicitInit.ts
@@ -4,12 +4,16 @@ import { fetch } from "../fetchWebSetup";
 
 const INIT_TEMPLATE = readFileSync(`${__dirname}/../../templates/hosting/init.js`, "utf8");
 
+export interface ImplicitInitRequest {
+  projectNumber: number;
+}
+
 export interface ImplicitInitResponse {
   js: string;
   json: string;
 }
 
-export async function init(options: any): Promise<ImplicitInitResponse> {
+export async function init(options: ImplicitInitRequest): Promise<ImplicitInitResponse> {
   const config = await fetch(options);
   const configJson = JSON.stringify(config, null, 2);
   return {
--- src/test/hosting/implicitInit.spec.ts
+++ src/test/hosting/implicitInit.spec.ts
@@ -15,15 +15,15 @@ describe("implicitInit", () => {
   afterEach(() => sandbox.restore());
 
   it("should fetch the information about the project", async () => {
-    await init({});
+    await init({ projectNumber: 1 });
 
     sinon.assert.calledOnce(fetchWebSetup.fetch as sinon.SinonStub);
-    sinon.assert.calledWithExactly(fetchWebSetup.fetch as sinon.SinonStub, {});
+    sinon.assert.calledWithExactly(fetchWebSetup.fetch as sinon.SinonStub, { projectNumber: 1 });
   });
 
   it("should insert the config into the js value returned", async () => {
     (fetchWebSetup.fetch as sinon.SinonStub).resolves({ foo: "bar" });
-    const data = await init({});
+    const data = await init({ projectNumber: 1 });
 
     expect(data.js).to.contain(`"foo": "bar"`);
   });
